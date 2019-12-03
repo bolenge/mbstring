c2e62f8d7099e74b4b3f8432af1df539ab5937a1
Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
2019-12-03 13:27:20 +0100

    Updating

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-let bstring = require('./bstring/index')
+/**
+ * Permet de mettre la prémière lettre d'un texte en majuscule
+ * @param {String} text Le text à traiter
+ */
+exports.ucFirst = (text) => {
+    return text[0].toUpperCase() + text.substring(1)
+}
 
-console.log(bstring.ucFirst('moliso'));
+/**
+ * Vérifie si l'url passé en paramètre est valide
+ * @param {String} url
+ * @return {Boolean}
+ */
+exports.isUrlValid = (url) =>  {
+    var expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+    var regex = new RegExp(expression);
+
+    if (regex.test(url)) {
+        return true
+    }else {
+        return false
+    }
+}
