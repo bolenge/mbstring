62b9dd5dc2eb841aae68ae19acd7862a63e8ef3d
Peniel Dialu <dialundamapeniel@gmail.com>
2022-09-28 16:00:02 +0100

    feat(after): returns everything after the last occurrence of the given value in a string

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,17 @@
 export const ucFirst = (str: string) : string => {
   return str[0].toUpperCase() + str.substring(1)
 }
+
+/**
+ * Return the remainder of a string after the first occurrence of a given value.
+ *
+ * @param  {string}  subject
+ * @param  {string}  search
+ * @returns {string}
+ */
+ export const after = (subject: string, search: string): string => {
+  if (search === "") return subject;
+  return subject.indexOf(search) !== -1
+    ? subject.substring(subject.indexOf(search) + search.length)
+    : subject;
+};
