94342d21218a41ae799a707a6c9dcdf875f36d60
Peniel Dialu <dialundamapeniel@gmail.com>
2022-09-30 14:39:47 +0100

    test(after): add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,3 +7,11 @@ import * as mbstring from "./";
 test("should return strings with first character uppercase", () => {
   expect(mbstring.ucFirst("peniel")).toEqual("Peniel");
 });
+
+/**
+ * @function mbstring.after
+ */
+
+ test("should return everything after the given value in a string.", () => {
+  expect(mbstring.after("This is my name", "This is")).toEqual(" my name");
+});
