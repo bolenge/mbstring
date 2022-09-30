c7256a7a21a1ee12e3bc4d6697100083f4af1eb4
Peniel Dialu <dialundamapeniel@gmail.com>
2022-09-30 14:45:11 +0100

    test(betweenFirst): add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -12,7 +12,7 @@ test("should return strings with first character uppercase", () => {
  * @function mbstring.after
  */
 
- test("should return everything after the given value in a string.", () => {
+test("should return everything after the given value in a string.", () => {
   expect(mbstring.after("This is my name", "This is")).toEqual(" my name");
 });
 
@@ -20,7 +20,7 @@ test("should return strings with first character uppercase", () => {
  * @function mbstring.afterLast
  */
 
- test("should return everything after the last occurrence of the given value in a string.", () => {
+test("should return everything after the last occurrence of the given value in a string.", () => {
   expect(mbstring.afterLast("My name is wifi, your name is bil", "is")).toEqual(
     " bil"
   );
@@ -30,7 +30,7 @@ test("should return strings with first character uppercase", () => {
  * @function mbstring.before
  */
 
- test("should return everything before the given value in a string.", () => {
+test("should return everything before the given value in a string.", () => {
   expect(mbstring.before("This is my name", "my name")).toEqual("This is ");
 });
 
@@ -38,7 +38,7 @@ test("should return strings with first character uppercase", () => {
  * @function mbstring.beforeLast
  */
 
- test("should return everything before the last occurrence of the given value in a string.", () => {
+test("should return everything before the last occurrence of the given value in a string.", () => {
   expect(mbstring.beforeLast("This is my name", "is")).toEqual("This ");
 });
 
@@ -46,8 +46,16 @@ test("should return strings with first character uppercase", () => {
  * @function mbstring.between
  */
 
- test("should return the portion of a string between two given values", () => {
+test("should return the portion of a string between two given values", () => {
   expect(mbstring.between("This is my name", "This", "name")).toEqual(
     " is my "
   );
+});
+
+/**
+ * @function mbstring.betweenFirst
+ */
+
+test("should return the smallest possible portion of a string between two given values", () => {
+  expect(mbstring.betweenFirst("[a] bc [d]", "[", "]")).toEqual("a");
 });
